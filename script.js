61e0f3638267a78adad61a0918add3857b021cd8
Sebas <sebasgova04@gmail.com>
2026-03-08 12:22:45 -0500

    lógica del cálculo matemático

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function calcular(){
 
 var minutos = document.getElementById("minutos").value;
+var precio = document.getElementById("precio").value;
+var correcciones = document.getElementById("correcciones").value;
+
+var total = (minutos * precio) + (correcciones * 5);
