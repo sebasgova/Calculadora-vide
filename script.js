002e3cca7e7562f8ae06974fec21580c9c018b7a
Sebas <sebasgova04@gmail.com>
2026-03-08 12:30:50 -0500

    salida de resultados

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ var precio = document.getElementById("precio").value;
 var correcciones = document.getElementById("correcciones").value;
 
 var total = (minutos * precio) + (correcciones * 5);
+
+document.getElementById("resultado").innerHTML = "Costo total: $" + total;
+
+}
